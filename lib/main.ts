ffd605546a7fedf32fd846cbf74f3312b210e177
Jack Morrison <jackamorr@gmail.com>
2021-01-18 21:15:28 -0600

    fixed unnecessary Datatip Adapter import that could cause issues

--- lib/main.ts
+++ lib/main.ts
@@ -1,13 +1,4 @@
-import DatatipAdapter from 'atom-languageclient/build/lib/adapters/datatip-adapter'
-// import 'reflect-metadata'
-// import { container } from 'tsyringe'
 import { GoLanguageClient } from './go-languageclient'
-
-// const client: GoLanguageClient = container
-//     .register<DatatipAdapter>(DatatipAdapter, {
-//         useClass: DatatipAdapter,
-//     })
-//     .resolve(GoLanguageClient)
 
 const client: GoLanguageClient = new GoLanguageClient()
 
